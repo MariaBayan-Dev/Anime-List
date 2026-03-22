a1870cf0202156466027ad96c516a497ae53d1e3
SakuHeli0s <mariabayanescobar@gmail.com>
2026-03-22 13:09:17 +0100

    Revisión hecha. Pequeños cambios. Proyecto acabado.

--- main.js
+++ main.js
@@ -5,31 +5,37 @@ const listAnime = document.getElementById('list-anime')
 //FUNCION API
 
 const fetchAnimes = async () => {
-    const response = await fetch("https://api.jikan.moe/v4/top/anime")
+    try{
+        const response = await fetch("https://api.jikan.moe/v4/top/anime")
 
-    const data = await response.json()
-    console.log(data)
+        const data = await response.json()
+        console.log(data)
 
-    data.data.forEach((anime) => {
-        console.log(listAnime)
+        data.data.forEach((anime) => {
+            console.log(listAnime)
 
-        const generosHTML = anime.genres.map((genero) => `<span>${genero.name}</span>`).join("")
+            const generosHTML = anime.genres.map((genero) => `<span>${genero.name}</span>`).join("")
 
-        const cardHTML = `
+            const cardHTML = `
 
-        <li class="anime-card">
-        <img src="${anime.images.jpg.image_url}">
+            <li class="anime-card">
+            <img src="${anime.images.jpg.image_url}">
 
-            <div class="card-info">
-                <h2>${anime.title}</h2>
-                <strong>⭐ ${anime.score}</strong>
-                ${generosHTML}
-            </div>
-        </li>
-        `
+                <div class="card-info">
+                    <h2>${anime.title}</h2>
+                    <strong>⭐ ${anime.score}</strong>
+                    ${generosHTML}
+                </div>
+            </li>
+            `
 
-        listAnime.insertAdjacentHTML('beforeend', cardHTML)
-    })
+            listAnime.insertAdjacentHTML('beforeend', cardHTML)
+        })
+    }catch (error){
+        //Se ejecuta este código solo si algo falla
+        console.error("Error al cargar la lista de animes:", error)
+        listAnime.innerHTML = "<p>Vaya, algo ha salido mal cargando la lista de animes. Inténtalo de nuevo más tarde.</p>"
+    }
 }
 
 fetchAnimes()
